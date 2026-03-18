6482672d61e4fa35a3b1901ee1d52f3923dc644c
danielrarp <d4nielml@gmail.com>
2026-03-18 13:53:21 -0600

    cambio de diseño

--- js/script.js
+++ js/script.js
@@ -4,3 +4,21 @@ function verColeccion(){
         behavior: "smooth"
     });
 }
+
+
+function comprar(nombre, btn){
+
+let card = btn.closest(".card");
+let talla = card.querySelector(".talla").value;
+
+if(talla === ""){
+    alert("Selecciona una talla");
+    return;
+}
+
+let mensaje = `Hola quiero la playera ${nombre} talla ${talla}`;
+let url = `https://wa.me/521XXXXXXXXXX?text=${encodeURIComponent(mensaje)}`;
+
+window.open(url, "_blank");
+
+}
